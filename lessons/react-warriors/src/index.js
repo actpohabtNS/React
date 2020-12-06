12a33b2187a218772a11944f738c26060f6b87b6
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-12-06 12:46:19 +0200

    [ ADDED ] MovieItem

--- lessons/react-warriors/src/index.js
+++ lessons/react-warriors/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MovieItem(props) {
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.rate}</p>
+    </div>
+  )
+}
+
 function App() {
-  return <div>Hello React Warriors!</div>
+  return (
+    <div>
+      <MovieItem title="Title new" rate={8.5}/>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
