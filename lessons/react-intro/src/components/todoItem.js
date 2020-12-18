34476391edde8f5b4ef1510216b5ff9c79cae879
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-12-18 13:06:18 +0200

    [ REFACTORED ] returned to a functional component

--- lessons/react-intro/src/components/todoItem.js
+++ lessons/react-intro/src/components/todoItem.js
@@ -1,35 +1,26 @@
 import React from 'react'
 
-class TodoItem extends React.Component {
-    constructor() {
-        super()
+function TodoItem() {
 
-        this.state = {
+    const {item, toggleDone} = this.props
 
-        }
-    }
+    return (
+        <div className="todo-item">
+            <input
+                type="checkbox"
+                checked={item.completed}
+                id={item.id}
+                onClick={() => toggleDone(item.id)}
+            />
 
-    render() {
-        const {item, toggleDone} = this.props
-
-        return (
-            <div className="todo-item">
-                <input
-                    type="checkbox"
-                    checked={item.completed}
-                    id={item.id}
-                    onClick={() => toggleDone(item.id)}
-                />
-
-                <label
-                    htmlFor={item.id}
-                    className={item.completed ? "completed" : ""}
-                >
-                    {item.text}
-                </label>
-            </div>
-        )
-    }
+            <label
+                htmlFor={item.id}
+                className={item.completed ? "completed" : ""}
+            >
+            {item.text}
+            </label>
+        </div>
+    )
 }
 
 export default TodoItem
