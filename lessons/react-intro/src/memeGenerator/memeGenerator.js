17ced8c35b8f5067459f2a5a5ce990d054a87d0a
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-12-19 19:45:27 +0200

    [ DONE ] MemeGenerator form

--- lessons/react-intro/src/memeGenerator/memeGenerator.js
+++ lessons/react-intro/src/memeGenerator/memeGenerator.js
@@ -5,14 +5,55 @@ class MemeGenerator extends React.Component {
         super()
 
         this.state = {
-
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
+    }
+
+    componentDidMount = () => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+
+                this.setState({
+                    allMemeImgs: memes
+                })
+            })
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target
+
+        this.setState({
+            [name]:value
+        })
     }
 
     render() {
         return (
             <div>
-    
+                <form className="meme-form">
+                    <input 
+                        type="text"
+                        placeholder="Top Text"
+                        name="topText"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        type="text"
+                        placeholder="Bottom Text"
+                        name="bottomText"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                    />
+
+                    <button>Gen</button>
+                </form>
             </div>
         )
     }
