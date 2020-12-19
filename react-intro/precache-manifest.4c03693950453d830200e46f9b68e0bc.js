4dd577292bc5025df65ca2a9b69a4d8e61f728ec
Sazonov Nikita <actpohabt.ns@gmail.com>
2020-12-19 22:06:39 +0200

    Updates

---
+++ react-intro/precache-manifest.4c03693950453d830200e46f9b68e0bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2d6a7ab72b10fdfa852e60f30196f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83d149e52f713f73e44a",
+    "url": "./static/css/main.507c2517.chunk.css"
+  },
+  {
+    "revision": "8f0e1cb23aadb9eabfc0",
+    "url": "./static/js/2.45d081ae.chunk.js"
+  },
+  {
+    "revision": "83d149e52f713f73e44a",
+    "url": "./static/js/main.966b35ff.chunk.js"
+  },
+  {
+    "revision": "664bc085f656b8193a04",
+    "url": "./static/js/runtime-main.0dee920d.js"
+  }
+]);
