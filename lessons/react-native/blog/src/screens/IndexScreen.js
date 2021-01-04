1883d814a84b3300bf5bfac1d5d31b770403650b
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-04 16:12:51 +0200

    [ DONE ] deleting blog posts

--- lessons/react-native/blog/src/screens/IndexScreen.js
+++ lessons/react-native/blog/src/screens/IndexScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native'
+import { View, Text, Button, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
     return (
         <View>
@@ -17,10 +18,44 @@ const IndexScreen = () => {
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
-                renderItem={({ item }) => <Text>{item.title}</Text>}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                            style={styles.row}
+                        >
+                            <Text style={styles.text}>
+                                {item.title}
+                            </Text>
+
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather
+                                    name="trash"
+                                    style={styles.icon}
+                                />
+                            </TouchableOpacity>
+                        </TouchableOpacity>
+                    )
+                }}
             />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+        paddingVertical: 15,
+        paddingHorizontal: 10   
+    },
+    text: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
 
 export default IndexScreen
