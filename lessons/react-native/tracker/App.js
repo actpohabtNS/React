37958a8c040b4da0c44a58434d467df904f00b52
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-05 23:39:14 +0200

    [ DONE ] navigation boilerplate

--- lessons/react-native/tracker/App.js
+++ lessons/react-native/tracker/App.js
@@ -1,7 +1,27 @@
-import SignupScreen from './src/screens/SignupScreen'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import SignupScreen from './src/screens/SignupScreen'
 import SigninScreen from './src/screens/SigninScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import AccountScreen from './src/screens/AccountScreen'
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  })
+})
+
+export default createAppContainer(switchNavigator);
