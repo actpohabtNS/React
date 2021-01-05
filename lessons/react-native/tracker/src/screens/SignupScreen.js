3b44621dafa61e94507efe610f6bf3f8b9bbddbf
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-05 23:48:13 +0200

    [ DONE ] temporary button navigation

--- lessons/react-native/tracker/src/screens/SignupScreen.js
+++ lessons/react-native/tracker/src/screens/SignupScreen.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
     return (
         <View>
             <Text style={{ fontSize: 48 }}>
                 Signup Screen
             </Text>
+
+            <Button
+                title={'Go to Signin'}
+                onPress={() => navigation.navigate('Signin')}
+            />
+
+            <Button
+                title={'Go to main flow'}
+                onPress={() => navigation.navigate('mainFlow')}
+            />
         </View>
     )
 }
--- lessons/react-native/tracker/src/screens/TrackListScreen.js
+++ lessons/react-native/tracker/src/screens/TrackListScreen.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
     return (
         <View>
             <Text style={{ fontSize: 48 }}>
                 TrackList Screen
             </Text>
+
+            <Button
+                title={'Go to Track Detail'}
+                onPress={() => navigation.navigate('TrackDetail')}
+            />
         </View>
     )
 }
