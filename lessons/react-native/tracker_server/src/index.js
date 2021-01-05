e79ed5438b6866afeab49914a114862d7c505fbe
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-05 22:10:57 +0200

    [ DONE ] JWT token | password + salt hashing

--- lessons/react-native/tracker_server/src/index.js
+++ lessons/react-native/tracker_server/src/index.js
@@ -1,8 +1,9 @@
 require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const authRoutes = require('./routes/authRoutes')
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express(); // our app that manages all requests
 
@@ -22,10 +23,10 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongoose: ', err)
 })
 
-app.get('/', (req, res) => { // every time get request is made, this func in invoked
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => { // every time get request is made, requireAuth middleware is invoked and if everything is ok, anonymus func in invoked
+    res.send(`Your email: ${req.user.email}`);
 })
 
 app.listen(3000, () => {
-    console.log('Listening in port 3000');
+    console.log('Listening to port 3000');
 })
