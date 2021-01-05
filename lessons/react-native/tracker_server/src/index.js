a317e1900c40cabb1158d9c631bd9cf41c9158b1
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-05 15:15:15 +0200

    [ DONE ] adding new users

--- lessons/react-native/tracker_server/src/index.js
+++ lessons/react-native/tracker_server/src/index.js
@@ -1,7 +1,13 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express(); // our app that manages all requests
+
+app.use(bodyParser.json());
+app.use(authRoutes); // associates all requests from 'authRoutes' with our app
 
 const mongoUri = "mongodb+srv://admin:passwordpassword@cluster0.htvoh.mongodb.net/tracker?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
@@ -16,7 +22,7 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongoose: ', err)
 })
 
-app.get('/', (req, res) => { // every time get request is made 
+app.get('/', (req, res) => { // every time get request is made, this func in invoked
     res.send('Hi there!');
 })
 
