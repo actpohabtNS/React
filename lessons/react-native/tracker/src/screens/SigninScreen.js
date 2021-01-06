2f07a30315ab7a9d316617ee424a1b88b7f19898
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-06 22:32:06 +0200

    [ DONE ] sign in page

--- lessons/react-native/tracker/src/screens/SigninScreen.js
+++ lessons/react-native/tracker/src/screens/SigninScreen.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Context as AuthContext } from '../context/AuthContext'
 
-const SigninScreen = () => {
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+import Spacer from '../components/Spacer'
+
+
+const SigninScreen = ({ navigation }) => {
+    const { state, signin } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>
-                Signin Screen
-            </Text>
+        <View style={styles.container}>
+            <AuthForm
+                headerText="Sign In to Your Account"
+                errorMessage=""
+                submitButtonText="Sign In"
+                onSubmit={signin}
+            />
+
+            <Spacer />
+
+            <NavLink
+                text="Don't have an account yet? Sign up!"
+                routeName="Signup"
+            />
         </View>
     )
 }
+
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 30
+    }
+})
 
 export default SigninScreen;
