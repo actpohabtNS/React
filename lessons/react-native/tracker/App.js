b87857a719cffb4e8aa5cca752abc64878d80be7
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-06 23:29:53 +0200

    [ DONE ] empty screen  while resolving at start

--- lessons/react-native/tracker/App.js
+++ lessons/react-native/tracker/App.js
@@ -3,6 +3,7 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
 import SignupScreen from './src/screens/SignupScreen'
 import SigninScreen from './src/screens/SigninScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
@@ -14,6 +15,7 @@ import { Provider as AuthProvider } from './src/context/AuthContext'
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
+  ResolveAuth: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen
@@ -26,6 +28,8 @@ const switchNavigator = createSwitchNavigator({
     TrackCreate: TrackCreateScreen,
     Account: AccountScreen,
   })
+}, {
+  initialRouteName: 'ResolveAuth'
 })
 
 const App = createAppContainer(switchNavigator);
