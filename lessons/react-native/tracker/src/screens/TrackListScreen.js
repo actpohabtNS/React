9347c4e6c11f20f898a05e17e399bdc309ff4754
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-07 21:09:29 +0200

    [ DONE ] showing tracks list, track's details

--- lessons/react-native/tracker/src/screens/TrackListScreen.js
+++ lessons/react-native/tracker/src/screens/TrackListScreen.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { View, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, ListItem } from 'react-native-elements'
+import { NavigationEvents } from 'react-navigation'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Context as TrackContext } from '../context/TrackContext'
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext);
+
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <NavigationEvents onWillFocus={fetchTracks} />
+
+            <Text h3>
                 TrackList Screen
             </Text>
 
-            <Button
-                title={'Go to Track Detail'}
-                onPress={() => navigation.navigate('TrackDetail')}
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })}>
+                            <ListItem bottomDivider>
+                                <ListItem.Content>
+                                    <ListItem.Title>{item.name}</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron />
+                            </ListItem>
+                        </TouchableOpacity>
+                    )
+                }}
             />
-        </View>
+        </SafeAreaView>
     )
 }
 
