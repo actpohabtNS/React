730ca173a37e2ebba9a3ae75c9ecbb1df07628b5
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-08 19:16:34 +0200

    [ DONE ] header component

---
+++ lessons/react-native/albums/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({text}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default Header;
