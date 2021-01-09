895b7a1344774db868536f0019672a3e21db3b33
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-09 16:04:35 +0200

    [ MOVED ] firebase app init

--- lessons/react-native/auth/App.js
+++ lessons/react-native/auth/App.js
@@ -6,19 +6,7 @@ import LoginForm from './src/components/LoginForm'
 
 export default function App() {
   useEffect(() => {
-    if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: "AIzaSyC9KeWREosmd889TJotvSCtChiPUH8Svjo",
-        authDomain: "auth-4d7d6.firebaseapp.com",
-        projectId: "auth-4d7d6",
-        storageBucket: "auth-4d7d6.appspot.com",
-        messagingSenderId: "676087308411",
-        appId: "1:676087308411:web:85cf06229e5aee408382de",
-        measurementId: "G-9C5K2XSNM5"
-      });
-    } else {
-      firebase.app();
-    }
+    
   }, []);
 
   return (
--- lessons/react-native/auth/src/hooks/useAuth.js
+++ lessons/react-native/auth/src/hooks/useAuth.js
@@ -2,6 +2,20 @@ import { useState } from 'react';
 import firebase from 'firebase';
 
 export default (setEmail, setPassword) => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC9KeWREosmd889TJotvSCtChiPUH8Svjo",
+      authDomain: "auth-4d7d6.firebaseapp.com",
+      projectId: "auth-4d7d6",
+      storageBucket: "auth-4d7d6.appspot.com",
+      messagingSenderId: "676087308411",
+      appId: "1:676087308411:web:85cf06229e5aee408382de",
+      measurementId: "G-9C5K2XSNM5"
+    });
+  } else {
+    firebase.app();
+  }
+
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   
