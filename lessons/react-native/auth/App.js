bafb67a5f62a5ca37733f63d70c659f08d413a75
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-09 15:55:33 +0200

    [ FIXED ] bug with multiple firebases

--- lessons/react-native/auth/App.js
+++ lessons/react-native/auth/App.js
@@ -6,15 +6,19 @@ import LoginForm from './src/components/LoginForm'
 
 export default function App() {
   useEffect(() => {
-    firebase.initializeApp({
-      apiKey: "AIzaSyC9KeWREosmd889TJotvSCtChiPUH8Svjo",
-      authDomain: "auth-4d7d6.firebaseapp.com",
-      projectId: "auth-4d7d6",
-      storageBucket: "auth-4d7d6.appspot.com",
-      messagingSenderId: "676087308411",
-      appId: "1:676087308411:web:85cf06229e5aee408382de",
-      measurementId: "G-9C5K2XSNM5"
-    });
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyC9KeWREosmd889TJotvSCtChiPUH8Svjo",
+        authDomain: "auth-4d7d6.firebaseapp.com",
+        projectId: "auth-4d7d6",
+        storageBucket: "auth-4d7d6.appspot.com",
+        messagingSenderId: "676087308411",
+        appId: "1:676087308411:web:85cf06229e5aee408382de",
+        measurementId: "G-9C5K2XSNM5"
+      });
+    } else {
+      firebase.app();
+    }
   }, []);
 
   return (
