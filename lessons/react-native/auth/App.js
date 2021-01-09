eef6bd863f0962357fec4049e7a480e70143a27d
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-09 11:30:01 +0200

    [ DONE ] firebase added

--- lessons/react-native/auth/App.js
+++ lessons/react-native/auth/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Header } from './src/components/common'
+import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './src/components/common';
 
 export default function App() {
+  useEffect(() => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC9KeWREosmd889TJotvSCtChiPUH8Svjo",
+      authDomain: "auth-4d7d6.firebaseapp.com",
+      projectId: "auth-4d7d6",
+      storageBucket: "auth-4d7d6.appspot.com",
+      messagingSenderId: "676087308411",
+      appId: "1:676087308411:web:85cf06229e5aee408382de",
+      measurementId: "G-9C5K2XSNM5"
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <View>
       <Header text="Authentication" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
