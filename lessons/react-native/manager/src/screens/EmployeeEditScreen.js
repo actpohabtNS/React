59f3baf3cd6b9857b2ea6126cc3e02ead3e3d4bc
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-11 19:05:40 +0200

    [ DONE ] text schedule

--- lessons/react-native/manager/src/screens/EmployeeEditScreen.js
+++ lessons/react-native/manager/src/screens/EmployeeEditScreen.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React, { useEffect } from 'react'
 import { Card, CardSection, Button } from '../components/common'
 import { connect } from 'react-redux'
+import Communications from 'react-native-communications'
 
 import EmployeeForm from '../components/EmployeeForm'
 import { employeeUpdate, employeeSave } from '../actions'
@@ -23,6 +24,13 @@ const EmployeeEdit = ({ employee, name, phone, shift, employeeUpdate, employeeSa
           Save Changes
         </Button>
       </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Communications.text(phone, `Your upcoming shift is on ${shift}`)}>
+          Text Schedule
+        </Button>
+      </CardSection>
+
       <CardSection>
         <Button>
           Fire
